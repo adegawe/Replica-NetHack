6e27f7f071877f5d053324c6a4dc8cfb7ed6f086
adegawe <95639704+adegawe@users.noreply.github.com>
2026-03-11 14:23:53 +0000

    Dungeon클래스 완성

--- RepHack/Dungeon.cs
+++ RepHack/Dungeon.cs
@@ -2,6 +2,9 @@ struct Room
 {
     public int x, y;
     public int width, length;
+
+    public int CenterX => x + width / 2;
+    public int CenterY => y + length / 2;
 }
 
 class Dungeon
@@ -24,60 +27,82 @@ class Dungeon
 
     public void InitDungeon()
     {
-        
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < length; j++)
+            {
+                map[i, j] = '#';
+            }
+        }
+        int roomNum = random.Next(3, 7);
+        CreateRoom(roomNum);
+        for(int i = 0; i < roomNum - 1; i++)
+        {
+            CreateCorridor(roomList[i].CenterX, roomList[i].CenterY, roomList[i+1].CenterX, roomList[i+1].CenterY);
+        }
     }
 
     public void CreateRoom(int roomCount)
     {
-        while(roomCount > 0){
+        int tryNum = 0;
+        while(roomCount > 0 && tryNum < 100){
             int x = random.Next(0, 70);
             int y = random.Next(0, 70);
             int roomWidth = random.Next(15, 30);
             int roomLength = random.Next(15, 30);
-            bool flag = true;
 
-            foreach (Room room in roomList)
+            if(!IsCanBuild(x, y, roomWidth, roomLength))
             {
-                if(!IsCanBuild(x, y))
+                tryNum++;
+                continue;
+            }
+
+            for(int i = x; i < x + roomWidth; i++)
+            {
+                for(int j = y; j < y + roomLength; j++)
                 {
-                    flag = false;
-                    break;
+                    map[i, j] = '.';
                 }
             }
-            if (flag)
+            Room room = new Room
             {
-                for(int i = x; i < x + roomWidth; i++)
-                {
-                    for(int j = y; j < y + roomLength; j++)
-                    {
-                        map[i, j] = '.';
-                    }
-                }
-                Room room = new Room();
-                room.x = x;
-                room.y = y;
-                room.width = width;
-                room.length = length;
-                roomList.Add(room);
-                roomCount--;
+                x = x,
+                y = y,
+                width = roomWidth,
+                length = roomLength
+            };
+            roomList.Add(room);
+            roomCount--;
             }
-        }
     }
     
-    public bool IsCanBuild(int x, int y)
+    public bool IsCanBuild(int x, int y, int newWidth, int newLength)
     {
         foreach(Room room in roomList)
         {
-            if(room.x + room.width > x || room.y + room.length)
+            if(room.x + room.width > x && room.x < x + newWidth && room.y + room.length > y &&  room.y < y + newLength)
             {
-                return true;
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     public void CreateCorridor(int Ax, int Ay, int Bx, int By)
     {
-        
+        int start = Math.Min(Ax, Bx);
+        int end = Math.Max(Ax, Bx);
+        for(int i = start; i <= end; i++)
+        {
+            map[i, Ay] = '.';
+        }
+
+        start = Math.Min(Ay, By);
+        end = Math.Max(Ay, By);
+
+        for(int j = start; j <= end; j++)
+        {
+            map[Bx, j] = '.';
+        }
     }
 }
---
+++ RepHack/Program.cs
@@ -0,0 +1,15 @@
+class Program
+{
+    static void Main()
+    {
+        Dungeon dungeon = new Dungeon();
+        dungeon.InitDungeon();
+        for(int i = 0; i < dungeon.length; i++)
+        {
+            for(int j = 0; j < dungeon.width; j++)
+            {
+                Console.Write(dungeon.map[i, j]);
+            }
+        }
+    }
+}
